6423d4fad2f75dcf4533fec214f3b8599955f5d0
Gilbert Fine <gilbert.fine@gmail.com>
2015-02-24 01:51:02 +0800

    import models defined in sdk

--- Model.hs
+++ Model.hs
@@ -2,10 +2,17 @@ module Model where
 
 import ClassyPrelude.Yesod
 import Database.Persist.Quasi
+import Database.Persist.Sql
+
+import WeiXin.PublicPlatform.Yesod.Site.Data
 
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
 -- at:
 -- http://www.yesodweb.com/book/persistent/
-share [mkPersist sqlSettings, mkMigrate "migrateAll"]
+share [mkPersist sqlSettings, mkMigrate "migrateAllMaster"]
+    $
     $(persistFileWith lowerCaseSettings "config/models")
+
+migrateAll :: Migration
+migrateAll = migrateAllMaster >> migrateAllWxppSubModels
